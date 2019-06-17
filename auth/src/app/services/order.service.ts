553ef2a7ec23205a946acfc5a3445862a012d6f1
soumyadip007 <soumyadip.note@gmail.com>
2019-06-18 00:09:38 +0530

    Auto Auth added

--- auth/src/app/services/order.service.ts
+++ auth/src/app/services/order.service.ts
@@ -2,23 +2,26 @@ import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
 import { on } from 'cluster';
+import { AuthHttp } from 'angular2-jwt';
 
 @Injectable()
 export class OrderService {
 
-  constructor(private http: Http) {
+  constructor(private authhttp: AuthHttp,private http: Http) {
   }
 
-  getOrders() { 
-
-    let headers= new Headers();
-    let token=localStorage.getItem('token');
-    headers.append('Authprization', 'Bearer'+token);
-
-    let option=new RequestOptions({headers:headers});
+  getOrders() 
+  {
 
 
-    return this.http.get('/api/orders')
+    // let headers= new Headers();
+    // let token=localStorage.getItem('token');
+    // headers.append('Authprization', 'Bearer'+token);
+
+    // let option=new RequestOptions({headers:headers});
+
+
+    return this.authhttp.get('/api/orders')
       .map(response => response.json());
   }
 
