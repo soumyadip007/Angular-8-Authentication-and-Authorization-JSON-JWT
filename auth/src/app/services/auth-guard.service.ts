a7237b634f35d1b1ee98142885ab0269fbd63621
soumyadip007 <soumyadip.note@gmail.com>
2019-06-17 22:48:24 +0530

    canActivate() added

--- auth/src/app/services/auth-guard.service.ts
+++ auth/src/app/services/auth-guard.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService: AuthService) { }
+
+  canActivate(){
+
+    if( this.authService.isLoggedIn()) return true;
+
+    this.router.navigate(['/login']);
+    return false;
+
+  }
 }
