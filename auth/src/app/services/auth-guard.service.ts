56de129ed5ed87f93e30da9cf9564b1b8defc38f
soumyadip007 <soumyadip.note@gmail.com>
2019-06-17 22:53:55 +0530

    RouterStateSnapshot added

--- auth/src/app/services/auth-guard.service.ts
+++ auth/src/app/services/auth-guard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,11 +11,11 @@ export class AuthGuard {
     private router: Router,
     private authService: AuthService) { }
 
-  canActivate(){
+  canActivate(route, state: RouterStateSnapshot){
 
     if( this.authService.isLoggedIn()) return true;
 
-    this.router.navigate(['/login']);
+    this.router.navigate(['/login'],)
     return false;
 
   }
