ac7943c316b057cf9d2194047b46051c8b61dac9
soumyadip007 <soumyadip.note@gmail.com>
2019-06-18 00:07:09 +0530

    OrderService config

--- auth/src/app/services/order.service.ts
+++ auth/src/app/services/order.service.ts
@@ -1,6 +1,7 @@
-import { Http } from '@angular/http';
+import { Http, RequestOptions, Headers } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+import { on } from 'cluster';
 
 @Injectable()
 export class OrderService {
@@ -9,7 +10,18 @@ export class OrderService {
   }
 
   getOrders() { 
+
+    let headers= new Headers();
+    let token=localStorage.getItem('token');
+    headers.append('Authprization', 'Bearer'+token);
+
+    let option=new RequestOptions({headers:headers});
+
+
     return this.http.get('/api/orders')
       .map(response => response.json());
   }
+
+
+
 }
