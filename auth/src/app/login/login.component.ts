c9a135a2e98341fdbb50daf48301daf9f730f7ec
soumyadip007 <soumyadip.note@gmail.com>
2019-06-17 19:24:13 +0530

    json config

--- auth/src/app/login/login.component.ts
+++ auth/src/app/login/login.component.ts
@@ -9,7 +9,7 @@ import { Router } from "@angular/router";
 })
 export class LoginComponent {
   invalidLogin: boolean; 
-
+zzzzz
   constructor(
     private router: Router, 
     private authService: AuthService) { }
